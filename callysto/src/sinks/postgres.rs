9b9cb2dab9696e8b16bc283cbd991f086979ab30
Ashwin Nirantar <ashwin.nirantar@rokt.com>
2024-05-06 12:42:15 -0700

    update: logs

--- callysto/src/sinks/postgres.rs
+++ callysto/src/sinks/postgres.rs
@@ -90,7 +90,7 @@ where
                 .unwrap_or_else(|err| panic!("Error connecting to the database: {}", err))
         });
 
-        info!("using clone version of postgres library sink v1.5");
+        info!("using clone version of postgres library sink v1.9.6");
 
         let (tx, rx) = crossbeam_channel::unbounded::<CPostgresRow<T>>();
         let (tx, rx) = (ArchPadding::new(tx), ArchPadding::new(rx));
@@ -101,7 +101,7 @@ where
         let client = Arc::new(pgpool);
         info!("created pointer to pgpool");
 
-        let data_sink = tokio::spawn(async move {
+        let data_sink = nuclei::spawn(async move {
             info!("creating a new nuclei spawn");
             let mut loop_entered = false; // Flag to track if the loop is entered
             while let Ok(item) = rx.recv() {
